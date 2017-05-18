ccd1aaa1fc9903d7bbc22e147d5f7b09b208b07b
tibo-debrabander <tibo.debrabander@student.scheppersinstituut.be>
2017-05-18 09:43:06 +0200

    bestanden in de github map gestoken

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+//LIJKT NIETS TE DOEN
+// var items = [
+//     ["logon m.debeucker", "pc CSV-01147", "datum: do 01/09/2016 uur:  9:20:14,36"],
+//     ["logon D.Rotolo", "pc CSV-01588", "datum: do 01/09/2016 uur:  9:33:54,33"],
+//     ["logon j.lormans", "pc CSV-01567", "datum: do 01/09/2016 uur: 12:36:37,13"],
+//     ["logon w.gevers", "pc CSV-02041", "datum: do 01/09/2016 uur: 12:58:13,56"]
+// ];
+// console.log(items[0][0]); // 1
+//console.log(items);
+
+//VERSIE 2
